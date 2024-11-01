f32b953c210c63b1b6bad81023ea0860d025e204
LuiLoureiro <luivloureiro@gmail.com>
2024-11-01 18:17:23 -0300

    Feat: Add a Firebase in Application

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
+require('dotenv').config();
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
